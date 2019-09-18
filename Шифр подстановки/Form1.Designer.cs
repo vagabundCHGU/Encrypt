b23689ba570e47fe711b1d83a6d33e7435da9bac
Иван Гулькин <nekcit98@gmail.com>
2019-09-18 12:51:13 +0300

    Первый шифр

--- Шифр подстановки/Form1.Designer.cs
+++ Шифр подстановки/Form1.Designer.cs
@@ -28,114 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.EncryptText = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.InputText = new System.Windows.Forms.TextBox();
-            this.EncryptDecryptButton = new System.Windows.Forms.Button();
-            this.DecryptText = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
             this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DecryptText = new System.Windows.Forms.TextBox();
+            this.EncryptDecryptButton = new System.Windows.Forms.Button();
+            this.InputText = new System.Windows.Forms.TextBox();
+            this.EncryptText = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // EncryptText
+            // tabControl1
             // 
-            this.EncryptText.Location = new System.Drawing.Point(168, 66);
-            this.EncryptText.Name = "EncryptText";
-            this.EncryptText.Size = new System.Drawing.Size(253, 20);
-            this.EncryptText.TabIndex = 1;
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(497, 215);
+            this.tabControl1.TabIndex = 9;
             // 
-            // label1
+            // tabPage1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(165, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(104, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Шифр подстановки";
-            // 
-            // InputText
-            // 
-            this.InputText.Location = new System.Drawing.Point(168, 40);
-            this.InputText.Name = "InputText";
-            this.InputText.Size = new System.Drawing.Size(253, 20);
-            this.InputText.TabIndex = 3;
-            this.InputText.Leave += new System.EventHandler(this.InputText_Leave);
-            // 
-            // EncryptDecryptButton
-            // 
-            this.EncryptDecryptButton.Location = new System.Drawing.Point(134, 137);
-            this.EncryptDecryptButton.Name = "EncryptDecryptButton";
-            this.EncryptDecryptButton.Size = new System.Drawing.Size(169, 23);
-            this.EncryptDecryptButton.TabIndex = 4;
-            this.EncryptDecryptButton.Text = "Зашифровать/Расшифровать";
-            this.EncryptDecryptButton.UseVisualStyleBackColor = true;
-            this.EncryptDecryptButton.Click += new System.EventHandler(this.Button1_Click);
-            // 
-            // DecryptText
-            // 
-            this.DecryptText.Location = new System.Drawing.Point(168, 92);
-            this.DecryptText.Name = "DecryptText";
-            this.DecryptText.Size = new System.Drawing.Size(253, 20);
-            this.DecryptText.TabIndex = 5;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(21, 47);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(92, 13);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Исходный текст:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(21, 73);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(126, 13);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Зашифрованный текст:";
+            this.tabPage1.BackColor = System.Drawing.Color.Salmon;
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.DecryptText);
+            this.tabPage1.Controls.Add(this.EncryptDecryptButton);
+            this.tabPage1.Controls.Add(this.InputText);
+            this.tabPage1.Controls.Add(this.EncryptText);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(489, 189);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Шифр подстановки";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(21, 99);
+            this.label4.Location = new System.Drawing.Point(51, 94);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(132, 13);
-            this.label4.TabIndex = 8;
+            this.label4.TabIndex = 16;
             this.label4.Text = "Расшифрованный текст:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Зашифрованный текст:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Исходный текст:";
+            // 
+            // DecryptText
+            // 
+            this.DecryptText.Location = new System.Drawing.Point(198, 87);
+            this.DecryptText.Name = "DecryptText";
+            this.DecryptText.Size = new System.Drawing.Size(253, 20);
+            this.DecryptText.TabIndex = 13;
+            // 
+            // EncryptDecryptButton
+            // 
+            this.EncryptDecryptButton.Location = new System.Drawing.Point(164, 132);
+            this.EncryptDecryptButton.Name = "EncryptDecryptButton";
+            this.EncryptDecryptButton.Size = new System.Drawing.Size(169, 23);
+            this.EncryptDecryptButton.TabIndex = 12;
+            this.EncryptDecryptButton.Text = "Зашифровать/Расшифровать";
+            this.EncryptDecryptButton.UseVisualStyleBackColor = true;
+            this.EncryptDecryptButton.Click += new System.EventHandler(this.EncryptDecryptButton_Click);
+            // 
+            // InputText
+            // 
+            this.InputText.Location = new System.Drawing.Point(198, 35);
+            this.InputText.Name = "InputText";
+            this.InputText.Size = new System.Drawing.Size(253, 20);
+            this.InputText.TabIndex = 11;
+            this.InputText.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.InputText_KeyPress);
+            this.InputText.Leave += new System.EventHandler(this.InputText_Leave_1);
+            // 
+            // EncryptText
+            // 
+            this.EncryptText.Location = new System.Drawing.Point(198, 61);
+            this.EncryptText.Name = "EncryptText";
+            this.EncryptText.Size = new System.Drawing.Size(253, 20);
+            this.EncryptText.TabIndex = 9;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(489, 189);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Шифр перестановки";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(427, 181);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.DecryptText);
-            this.Controls.Add(this.EncryptDecryptButton);
-            this.Controls.Add(this.InputText);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.EncryptText);
+            this.ClientSize = new System.Drawing.Size(497, 215);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Алгоритмы шифрования";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.TextBox EncryptText;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox InputText;
-        private System.Windows.Forms.Button EncryptDecryptButton;
-        private System.Windows.Forms.TextBox DecryptText;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox DecryptText;
+        private System.Windows.Forms.Button EncryptDecryptButton;
+        private System.Windows.Forms.TextBox InputText;
+        private System.Windows.Forms.TextBox EncryptText;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
 
--- Шифр подстановки/Form1.cs
+++ Шифр подстановки/Form1.cs
@@ -15,12 +15,12 @@ namespace Шифр_подстановки
         private Dictionary<char, string> keyValues = new Dictionary<char, string>(25);
         const int block = 2;
         private int count = 0;
-        private string alphabet = "abcdefghijklmnopqrstuvwx";
+        private string alphabet = "abcdefghijklmnopqrstuvwxy";
 
         private string[] table = {"00","01","02","03","04",
                                   "10","11","12","13","14",
                                   "20","21","22","23","24",
-                                  "30","31","31","33","34",
+                                  "30","31","32","33","34",
                                   "40","41","42","43","44"};
 
         public Form1()
@@ -38,25 +38,11 @@ namespace Шифр_подстановки
             return keyValues;
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            CreateTable();
-            InputText.Select();
-        }
-
-        private void Button1_Click(object sender, EventArgs e)
-        {
-            EncryptText.Text = null;
-            DecryptText.Text = null;
-            Encrypt(InputText.Text);
-            Decrypt(EncryptText.Text);
-        }
-        
         private void Encrypt(string text)
         {
             string key = "";
 
-            for (int i = 0; i < text.Length; i++) 
+            for (int i = 0; i < text.Length; i++)
             {
                 key += text[i];
                 if (key.Length == 2)
@@ -71,19 +57,42 @@ namespace Шифр_подстановки
         {
             for (int i = 0; i < text.Length; i++)
             {
-                if(text.Contains(keyValues.Where(x=>x.Key == text[i]).FirstOrDefault().Key))
+                if (text.Contains(keyValues.Where(x => x.Key == text[i]).FirstOrDefault().Key))
                 {
                     DecryptText.Text += keyValues.Where(x => x.Key == text[i]).FirstOrDefault().Value;
                 }
             }
         }
 
-        private void InputText_Leave(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CreateTable();
+            InputText.Select();
+        }
+
+        private void EncryptDecryptButton_Click(object sender, EventArgs e)
+        {
+            EncryptText.Text = null;
+            DecryptText.Text = null;
+            Encrypt(InputText.Text);
+            Decrypt(EncryptText.Text);
+        }
+
+        private void InputText_Leave_1(object sender, EventArgs e)
         {
             if (InputText.Text.Length % block != 0)
             {
                 InputText.Text += '0';
             }
+        }
+
+        private void InputText_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if (ch == '0' || ch == '1' || ch == '2' || ch == '3' || ch == '4')
+                e.Handled = false;
+            else
+                e.Handled = true;
         }
     }
 }
